bd9c2ef272c8ec0f6653f90c890a62a6bf87ea20
kay-15 <rnkhangweni@gmail.com>
2023-07-25 02:43:43 -0700

    fixing betty

--- 0-solution.c
+++ 0-solution.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - A function that prints formatted output:
  * function is declared with a variable
@@ -9,26 +8,21 @@
  * the number of characters printed
  * Return: The value of string
  */
-
 int _printf(const char *format, ...)
 {
-	int count;
-	va_list list;
-
-	/**
-	 * The function checks if format parameter is NULL
-	 * If NULL, function returns -1 indicating an error
-	 * If format is not NULL, function initializes the variable
-	 * argument (list) using (va_start) which allows us access
-	 * the variable argument in the format string
-	 */
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-	count = vprint(format, list);
-	va_end(list);
-
-	return (count);
+int count;
+va_list list;
+/**
+* The function checks if format parameter is NULL
+* If NULL, function returns -1 indicating an error
+* If format is not NULL, function initializes the variable
+* argument (list) using (va_start) which allows us access
+* the variable argument in the format string
+*/
+if (format == NULL)
+return (-1);
+va_start(list, format);
+count = vprint(format, list);
+va_end(list);
+return (count);
 }
